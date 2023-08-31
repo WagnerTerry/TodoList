0e95dd7fb2b8c366063939705c27936403eb3c1d
Wagner Terry <goncalveswagner15@gmail.com>
2023-08-31 12:30:07 -0300

    feat: iniciando projeto e configurando

---
+++ App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  Text,
+  View
+} from 'react-native';
+
+
+const App = () => {
+
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Hello World</Text>
+
+      </View>
+    </SafeAreaView>
+  );
+}
+
+export default App;
